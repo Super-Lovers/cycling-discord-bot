4506c0d2a2c572ab097c827c5cb357d81ae72ee3
Super-Lovers <n.v.ivanov@st.hanze.nl>
2020-07-07 17:35:42 +0300

    Made retrieving sessions from history

--- index.js
+++ index.js
@@ -40,10 +40,27 @@ clientDiscord.on('message', (message) => {
 	 * based on the given session/s date and displays them.
 	 */
 	if (command == '$getSession') {
-		const commandArguments = message.content.split(' ');
-		const sessionDay = commandArguments[1];
-		const sessionMonth = commandArguments[2];
-		const sessionYear = commandArguments[3];
+		// Retrieving the title from the command string
+		let indexOfFirstQuote = 0;
+		let indexOfLastQuote = 0;
+
+		let isFirstQuoteFound = false;
+
+		const messageText = message.content;
+		for (let i = 0; i < messageText.length; i++) {
+			if (messageText[i] == '"' && isFirstQuoteFound == false) {
+				indexOfFirstQuote = i;
+				isFirstQuoteFound = true;
+			}
+
+			if (messageText[i] == '"' && isFirstQuoteFound == true) {
+				indexOfLastQuote = i;
+			}
+		}
+
+		const commandArguments = (messageText.substring(0, indexOfFirstQuote - 1) + messageText.substring(indexOfLastQuote + 1, messageText.length)).split(' ');
+
+		const sessionTitle = messageText.substring(indexOfFirstQuote, indexOfLastQuote + 1);
 		const foundSessions = [];
 
 		clientMongo.connect(async () => {
@@ -52,15 +69,26 @@ clientDiscord.on('message', (message) => {
 
 			for (let i = 0; i < sessions.length; i++) {
 				const element = sessions[i];
+
 				// 15:38 - 2/6/2020 => [15:38 ], [ 2/6/2020] => [ 2, 6, 2020]
 				const elementDate = element.date.split('-')[1].split('/');
 				const elementDay = parseInt(elementDate[0]);
 				const elementMonth = parseInt(elementDate[1]);
 				const elementYear = parseInt(elementDate[2]);
+				const elementTitle = element.title;
 
-				if (sessionDay == elementDay &&
-					sessionMonth == elementMonth &&
-					sessionYear == elementYear) {
+				if (indexOfFirstQuote == 0) {
+					const sessionDay = commandArguments[1];
+					const sessionMonth = commandArguments[2];
+					const sessionYear = commandArguments[3];
+
+					if (sessionDay == elementDay &&
+						sessionMonth == elementMonth &&
+						sessionYear == elementYear) {
+
+						foundSessions.push(element);
+					}
+				} else if (sessionTitle == elementTitle) {
 
 					foundSessions.push(element);
 				}
@@ -69,11 +97,16 @@ clientDiscord.on('message', (message) => {
 			if (foundSessions.length == 0) {
 				message.reply(' no sessions were found ❌');
 			} else {
-				message.reply(' ' + foundSessions.length + ' sessions were found ✅\n');
+				if (foundSessions.length == 1) {
+					message.reply(' ' + foundSessions.length + ' session was found ✅\n');
+				} else if (foundSessions.length > 1) {
+					message.reply(' ' + foundSessions.length + ' sessions were found ✅\n');
+				}
 
 				for (let i = 0; i < foundSessions.length; i++) {
 					const element = foundSessions[i];
-					message.reply(getSessionString(element));
+
+					sendMessageToChannel(generalChannelID, (getSessionString(element)));
 				}
 			}
 		});
@@ -84,17 +117,37 @@ clientDiscord.on('message', (message) => {
 	 * into the mongoDB instance for that specific user.
 	 */
 	if (command == '$addSession') {
-		const commandArguments = message.content.split(' ');
 		const currentDate = new Date();
 		const currentDateFormat = currentDate.getHours() + ':' + currentDate.getMinutes() + ' - ' + currentDate.getDay() + '/' + currentDate.getMonth() + '/' + currentDate.getFullYear();
 
+		// Retrieving the title from the command string
+		let indexOfFirstQuote = 0;
+		let indexOfLastQuote = 0;
+
+		let isFirstQuoteFound = false;
+
+		const messageText = message.content;
+		for (let i = 0; i < messageText.length; i++) {
+			if (messageText[i] == '"' && isFirstQuoteFound == false) {
+				indexOfFirstQuote = i;
+				isFirstQuoteFound = true;
+			}
+
+			if (messageText[i] == '"' && isFirstQuoteFound == true) {
+				indexOfLastQuote = i;
+			}
+		}
+
+		const commandArguments = (messageText.substring(0, indexOfFirstQuote - 1) + messageText.substring(indexOfLastQuote + 1, messageText.length)).split(' ');
+
 		const session = {
-			date: currentDateFormat,
+			title: messageText.substring(indexOfFirstQuote, indexOfLastQuote + 1),
 			calories: commandArguments[1],
 			distanceTravelled: commandArguments[2],
 			averageSpeed: commandArguments[3],
 			maxSpeed: commandArguments[4],
 			duration: commandArguments[5],
+			date: currentDateFormat,
 			author: message.author.username,
 		};
 
@@ -175,8 +228,8 @@ clientDiscord.on('message', (message) => {
 	if (message.content == '$help') {
 		message.reply(
 			' here are the commands: \n' +
-			'**$addSession** ``calories`` ``distance`` ``average speed`` ``max speed`` ``duration``\n' +
-			'**$getSession** ``day`` ``month`` ``year``\n\n'
+			'**$addSession** ``"title in quotes"`` ``calories`` ``distance`` ``average speed`` ``max speed`` ``duration``\n' +
+			'**$getSession** ``["title in quotes"]`` ``day`` ``month`` ``year``\n\n'
 		);
 	}
 });
@@ -185,8 +238,11 @@ function sendMessageToChannel(channelId, message) {
 	clientDiscord.channels.cache.get(channelId).send(message);
 }
 
+/**
+ * Returns the given session's details in a well-formatted string
+ */
 function getSessionString(session) {
-	return 'Session **' + session.date + '**\n\n' +
+	return 'Session **' + session.title.substring(1, session.title.length - 1) + '** from [' + session.date + ']\n\n' +
 		'=> **Calories burned:** ``' + session.calories + '`` 🍕\n' +
 		'=> **Distance travelled:** ``' + session.distanceTravelled + ' km`` 📐\n' +
 		'=> **Average speed:** ``' + session.averageSpeed + ' km/ph`` 💨\n' +
